f1f5e36f2bddd8834d1b4c98995140613739d741
Lantz111 <osla1001@live.mark.se>
2018-08-30 14:47:28 +0200

    ex1.3

---
+++ 2018-08-30/ex1.3/ex1.3.cpp
@@ -0,0 +1,12 @@
+// exempel 1.3, sid 18
+#include <iostream>
+using namespace std;
+int main()
+{
+	
+	cout << "Tjena Mittbena" << endl;
+	cout << "Jonte är nästan lika snygg som Zannah" << endl;
+	cout << "Bara skojja, han är ful som stryk";
+	
+	return 0;
+}
