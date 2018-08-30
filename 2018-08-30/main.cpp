1926d4a697c0921b4b964497b7451dccdf9193f7
Lantz111 <osla1001@live.mark.se>
2018-08-30 14:09:27 +0200

    2 commit

---
+++ 2018-08-30/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+//
+using namespace std;
+int main()
+{
+	// cout är standard utenhet, skärmen
+	// << är utmatningsoperator
+    cout << "TJENIXEN";	
+    // programmet körs rad efter rad
+    // fungerar allt kommer vi fram
+    // till "return 0;"
+    // heltalet 0 returneras till operativsystemet
+	return 0;
+}
