4f9e21737330f8f4b1231902beaa3df10b96e14c
Lantz111 <osla1001@live.mark.se>
2018-09-06 14:39:28 +0200

    siffror o nummer

---
+++ 2018-09-06/Test.cpp
@@ -0,0 +1,27 @@
+// exempel 1.3, sid 18
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << " CCCCC" << endl;
+	cout << "C         +     +" << endl;
+	cout << "C       +++++ +++++" << endl;
+	cout << "C         +     +" << endl;
+	cout << " CCCCC" << endl;
+	cout << endl;
+	cout << endl;
+	
+	
+	cout << "     \" \" \" " << endl;
+	cout << "  \\\\       //" << endl;
+	cout << "    O     O" << endl;
+	cout << "       U" << endl;
+	cout << endl;
+	cout << "      ---" << endl;
+
+	
+	
+	
+	
+	
+}
---
+++ 2018-09-06/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+     // en variabel för heltal
+   int tal1;
+     // variabeln tilldelades ett värde
+   tal1 = 8;
+   cout << tal1 << endl;
+     // Flyttal, decimaltal
+     // OBS! skrivs med decimalpunkt  .
+   tal1 = 4.9;
+   cout << tal1 << endl;
+   float f1;
+   f1 = 4.9;
+   cout << f1 << endl;
+   f1 = (f1 * 3);
+   cout << f1 << endl;
+   
+   
+   
+  
+   
+   
+   
+   
+   
+   
+   return 0;		
+}
