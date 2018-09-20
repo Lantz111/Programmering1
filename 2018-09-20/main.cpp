2fb11a2159df2158aa315d4cd2dc0280a32635bf
Lantz111 <osla1001@live.mark.se>
2018-09-20 14:55:38 +0200

    2018-09-20

---
+++ 2018-09-20/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+  char tal1;
+  char tal2;
+  char åäö;
+  
+  cin >> tal1;
+  tal2 = tal1 + 1;
+  åäö = tal1 + 2;
+  cout << tal1 << tal2 << åäö << endl;
+  
+  cout << "åäö";
+  
+  return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
