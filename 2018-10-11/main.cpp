1ac07fc5db0aaf5250e8853bb5db9ac611fc90ff
Lantz111 <osla1001@live.mark.se>
2018-10-11 14:58:44 +0200

    2018-10-11

---
+++ 2018-10-11/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main ()
+{
+	//cout << "Tjena" << endl;
+	//int tal;
+	//tal = ( (4*2) + 1 );
+	//cout << tal << endl;
+	
+	//cout << sqrt(69) << endl;
+	
+	//int manader;
+	//cin >> manader; 
+	//cout << manader << " månader " << "= " << manader/12 << " år och " << manader%12 << " månader" << endl;
+	
+	cout << "Mata in ett fucking heltal" << endl;
+	int tal;
+	cin >> tal;
+	int tal1=tal;
+	tal++;
+	tal-=tal1;
+	cout << "Nu har du fått talet " << tal << endl;
+	
+ 	
+	
+	return 0;
+}
