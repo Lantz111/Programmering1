2dc57c6847ce1053759862e39d48a5ed110b5b7f
Lantz111 <osla1001@live.mark.se>
2018-10-18 14:23:55 +0200

    2018-10-18

---
+++ 2018-10-18/prov1.cpp
@@ -0,0 +1,64 @@
+//Oscar Lantz
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main ()
+{
+        //Uppgift 1
+
+	/*char tal1;
+	char tal2;
+	cout << "Mata in en bokstav liten bokstav" << endl;
+	cin >> tal1;
+	tal2 = tal1 - 32;
+	cout << "Här är versalen" << endl << tal2 << endl;*/
+	
+	    //Uppgift 2
+	    
+	/*float tal1;
+	float tal2;
+	float tal3;
+	float tal4;
+	cout << "Mata in ett tal med tre decimaler" << endl;
+	cin >> tal1;
+	cout << "Ett till" << endl;
+	cin >> tal2;
+	cout << "Ett till" << endl;
+	cin >> tal3;
+	cout << "Tack!" << endl;
+	tal4 = tal1 + tal2 + tal3;
+	cout << "Medelvärdet av " << tal1 << " " << tal2 << " och " << tal3 << " är " << fixed << setprecision(1) << tal4/3 << endl;*/
+	
+	    //Uppgift 3
+	    
+	
+	
+	    //Uppgift 4
+	    
+	    
+	int Timmar;
+	int Minuter;
+	int Sekunder;    
+	cout << "Mata in ett antal timmar, minuter och sekunder (använd heltal)" << endl;
+	cout << "Timmar: ";
+	cin >> Timmar;
+	cout << endl << "Minuter: ";
+	cin >> Minuter;
+	cout << endl << "Sekunder: ";
+	cin >> Sekunder;
+	//Timmar = Minuter / 60;
+	//Minuter = Sekunder / 60;
+	cout << "Den inmatade tiden motsvarar " << Timmar << " Timmar, " << endl;          
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
