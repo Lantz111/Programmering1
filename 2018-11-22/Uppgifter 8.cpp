89c395b8225fc8f06f4099a1bdaac3c47146a064
Lantz111 <osla1001@live.mark.se>
2018-11-22 14:55:57 +0100

    2018-11-22

---
+++ 2018-11-22/Uppgifter 8.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+	
+	//8.2
+	
+	
+	/*int tal = 3;
+	while (tal >=3 && tal <= 10) {
+		cout << tal << endl; 
+		tal++;
+	}
+	*/
+	
+	
+	//8.3
+	
+	
+	/*int tal = 3;
+	
+	do{
+		cout << tal << endl; 
+		tal++;
+	  }
+	while (tal <=10);*/
+	
+	
+	//8.5
+	
+	
+	char tal; 
+	cout << "Skriv antalet stjärnor du vill ha" << endl;
+	cin >> tal;
+	cout << tal << endl;
+	
+      return 0;
+}
---
+++ 2018-11-22/While.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int counter = 0;
+	while (counter < 10000000000000)
+	{
+		cout << counter << endl;
+		counter++;
+	}
+	
+	
+	return 0;	
+}
