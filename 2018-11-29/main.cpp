be23f156b8c35a087e0da81f5835b1bf0d01dbd5
Lantz111 <osla1001@live.mark.se>
2018-11-29 14:51:28 +0100

    2018-11-29

---
+++ 2018-11-29/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	//8.1
+	
+	/*
+	for(int n=3; n<=10; ++n)
+	{
+		cout << n << endl;
+	}
+	*/
+	
+	
+	//8.2
+	
+	/*
+	int n=3;
+	
+	while(n<=10)
+	{
+		cout << n << endl;
+		++n; 
+	}
+	*/
+	
+	
+      return 0;
+}
