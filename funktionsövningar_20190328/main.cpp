79ca3e12ef83d9b90f5f64de69c1ca6f6007d7db
Lantz111 <osla1001@live.mark.se>
2019-04-04 14:52:38 +0200

    uppgifter till ingemar

--- funktionsövningar_20190328/main.cpp
+++ funktionsövningar_20190328/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-void multiplikation (double falt[], int antal);
+/*void multiplikation (double falt[], int antal);
 int main(){
 	
 	double falt[] = {-4, -4.5, 6, 0};
@@ -33,4 +33,84 @@ void multiplikation (double falt[], int antal){
 	     falt[i] = falt [i] * -1;
 		
 	}
+}*/
+
+
+
+//Uppgift 2
+
+/*double hogsta (double falt[], int antal);
+int main()
+{
+	double falt[] = {48, 7, 9, 34};
+	
+	for (int i = 0; i < 4; i++){
+		
+	     	cout << falt[i] << " ";
+	   }
+	
+	cout << endl << hogsta(falt, 4);
+	
+	
+
+return 0;
 }
+
+double hogsta (double falt[], int antal){
+	
+	double temp = falt[0];
+	for (int i = 1; i < antal; i++){
+	if (falt[i] > temp){
+		temp = falt[i];
+	}
+}
+	return temp;
+}*/
+
+
+
+//Uppgift 3
+
+/*double area (double d);
+int main(){
+	
+	double d;
+	cout << "Skriv diametern (cm) på din cirkel: ";
+	cin >> d;
+	
+	double a = area(d);
+	
+	cout << endl << "Arean på din cirkel är " << a << " kvadratcentimeter!" << endl; 
+
+}
+
+double area (double d){
+	
+	double a = (3.14 * d * d) / 4; 	
+	
+	return a;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
